f5b9ecf6fa2905535c40182f70f51895fc92c5e2
Florencia Lafuente <flor@properati.com>
2020-06-08 19:23:58 -0300

    First test with enzyme

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Enzyme from 'enzyme';
+import Enzyme, { shallow } from 'enzyme';
 import EnzymeAdapter from 'enzyme-adapter-react-16';
 
 import App from './App';
@@ -7,3 +7,13 @@ import App from './App';
 Enzyme.configure({
   adapter: new EnzymeAdapter(),
 });
+
+test('renders without crashing', () => {
+  const wrapper = shallow(<App />);
+  /*
+    Console.log DOM as a string
+    console.log(wrapper.debug());
+  */
+  expect(wrapper).toBeTruthy();
+});
+
