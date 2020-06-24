32b4cc9f31d7701ab573c80c7a8ba188703b28dd
Florencia Lafuente <flor@properati.com>
2020-06-24 20:41:06 -0300

    refactor some reusable functions

--- src/App.test.js
+++ src/App.test.js
@@ -8,9 +8,30 @@ Enzyme.configure({
   adapter: new EnzymeAdapter(),
 });
 
+/**
+ * Factory function to create a ShallowWrapper for the App Component
+ * @function setup
+ * @param {object} props - Component props specific to this setup
+ * @param {*} state  - Initial state for setup
+ * @returns {ShallowWrapper}
+ */
+const setup = (props = {}, state = null) => {
+  return shallow(<App {...props} />);
+};
+
+/**
+ * Return Shallow Wrapper containing node(s) with the given data-test attr
+ * @param {ShallowWrapper} wrapper -Enzyme shallow wrapper to search within.
+ * @param {string} val - Value of data-test attribute for search.
+ * @returns {ShallowWrapper}
+ */
+const findByTestAttr = (wrapper, val) => {
+  return wrapper.find(`[data-test='${val}']`);
+};
+
 test('renders without crashing', () => {
-  const wrapper = shallow(<App />);
-  const appComponent = wrapper.find("[data-test='component-app']")
+  const wrapper = setup();
+  const appComponent = findByTestAttr(wrapper, 'component-app')
   /*
     Console.log DOM as a string
     console.log(wrapper.debug());
@@ -20,14 +41,14 @@ test('renders without crashing', () => {
 });
 
 test('renders increment button', () => {
-  const wrapper = shallow(<App />);
-  const button = wrapper.find("[data-test='component-increment-button']")
+  const wrapper = setup();
+  const button = findByTestAttr(wrapper, 'component-increment-button');
   expect(button.length).toBe(1);
 });
 
 test('renders counter display', () => {
-  const wrapper = shallow(<App />);
-  const counterDisplay = wrapper.find("[data-test='component-counter-display']")
+  const wrapper = setup();
+  const counterDisplay = findByTestAttr(wrapper, 'component-counter-display');
   expect(counterDisplay.length).toBe(1);
 });
 
