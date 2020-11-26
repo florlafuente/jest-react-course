773c90d9729b6a642bc353a74b5b6c4dc68b0e26
Florencia Lafuente <flor@properati.com>
2020-11-26 10:42:12 -0300

    add Congrats component & test utils

--- src/Congrats.js
+++ src/Congrats.js
@@ -1,9 +1,17 @@
 import React from 'react';
 
-export default () => {
-  return (
-    <div>
-      Congrats
-    </div>
-  )
+export default (props) => {
+    if (props.success) {
+      return (
+        <div data-test="component-congrats">
+          <span data-test="congrats-message">
+            Congratulations! You guessed the word!
+          </span>
+        </div>
+      )
+    } else {
+      return (
+        <div data-test="component-congrats" />
+      )
+    }
 };
